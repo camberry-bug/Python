20df320ccc1de18c8967c47fac0808f411f8acaf
Cambria Spangler <rnt2yx@virginia.edu>
2023-10-25 16:21:00 -0400

    used package for prime number determination

--- RSA_algo_keygen.py
+++ RSA_algo_keygen.py
@@ -1,27 +1,14 @@
 from primelibpy import Prime
+import sympy
 import random
 
-def is_prime(n):
-    """Check if a number is prime."""
-    if n <= 1:
-        return False
-    if n <= 3:
-        return True
-    if n % 2 == 0 or n % 3 == 0:
-        return False
-    i = 5
-    while i * i <= n:
-        if n % i == 0 or n % (i + 2) == 0:
-            return False
-        i += 6
-    return True
 def closest_prime(n):
     """Find the closest prime to a number."""
     distance = 0
     while True:
-        if is_prime(n + distance):
+        if sympy.isprime(n + distance):
             return n + distance
-        if is_prime(n - distance):
+        if sympy.isprime(n - distance):
             return n - distance
         distance += 1
 
