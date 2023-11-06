b0b68330e13eaa00f5c6cd745929f9dd4c241adf
Cambria Spangler <rnt2yx@virginia.edu>
2023-11-06 11:19:20 -0500

    updated RSA to use Mod inverse

--- RSA_algo_keygen.py
+++ RSA_algo_keygen.py
@@ -2,28 +2,35 @@ from primelibpy import Prime
 import sympy
 import math
 import random
+from sympy import mod_inverse
 
-def closest_prime(n):
+
+def closest_prime(num):
     distance = 0
     while True:
-        if sympy.isprime(n + distance):
-            return n + distance
-        if sympy.isprime(n - distance):
-            return n - distance
+        if sympy.isprime(num + distance):
+            return num + distance
+        if sympy.isprime(num - distance):
+            return num - distance
         distance += 1
+
 def is_coprime(a, b):
     return math.gcd(a, b) == 1
+
 def encrypt(Message, PU):
     M = int.from_bytes(Message.encode(), 'big')  # Convert string to integer
     Cyphertext = pow(M, PU[1], PU[0])
     return Cyphertext
+
 def decrypt(Cyphertext, PR):
     Message = pow(Cyphertext, PR[1], PR[0])
     return Message.to_bytes((Message.bit_length() + 7) // 8, 'big').decode()
+
 def encrypt_chunked(Message, PU):
     chunks = Message.split(' ')  # Split message into chunks based on spaces
     encrypted_chunks = [encrypt(chunk, PU) for chunk in chunks]
     return encrypted_chunks
+
 def decrypt_chunked(encrypted_chunks, PR):
     decrypted_chunks = [decrypt(chunk, PR) for chunk in encrypted_chunks]
     return ' '.join(decrypted_chunks)
@@ -46,19 +53,11 @@ e = 65537
 while not is_coprime(e, z):
     e += 2
 
-# Extended Euclidean Algorithm to find d
-m0, x0, x1 = z, 0, 1
-a, m = e, z
-while a > 0 and m > 0:
-    q = a // m
-    a, m = m, a % m
-    x0, x1 = x1 - q * x0, x0
-d = x1 % m0
+d = mod_inverse(e, z)
 
 PR = [n, d]
 PU = [n, e]
 
-# Test
 M = input("Enter message to encrypt:")
 print("Message = ", M)
 C = encrypt_chunked(M, PU)
